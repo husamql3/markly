71ac46ad231c5fb6ac1e83b660810daf47d0affb
Hüsam <husamahmud@gmail.com>
2025-06-04 23:52:28 +0300

    update tsconfig

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,9 @@
-import { log } from "@markly/utils"
-import { createServer } from "./server.js"
+import { log } from "@markly/utils";
+import { createServer } from "./server";
 
-const port = process.env.PORT || 8080
-const server = createServer()
+const port = process.env.PORT || 8080;
+const server = createServer();
 
 server.listen(port, () => {
-	log(`server running on ${port}`)
-})
+  log(`server running on ${port}`);
+});
