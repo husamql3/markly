60e9ba8169f95a775471f1e6855efcf290bdcc29
Hüsam <husamahmud@gmail.com>
2025-06-23 18:56:57 +0300

    implement reactQuery provider

---
+++ apps/client/src/providers/react-query-provider.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import {
+  QueryClient,
+  QueryClientProvider,
+  defaultShouldDehydrateQuery,
+} from "@tanstack/react-query";
+import SuperJSON from "superjson";
+import type { ReactNode } from "react";
+
+const createQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        // With SSR, we usually want to set some default staleTime
+        // above 0 to avoid refetching immediately on the client
+        staleTime: 30 * 1000,
+      },
+      dehydrate: {
+        serializeData: SuperJSON.serialize,
+        shouldDehydrateQuery: (query) =>
+          defaultShouldDehydrateQuery(query) ||
+          query.state.status === "pending",
+      },
+      hydrate: {
+        deserializeData: SuperJSON.deserialize,
+      },
+    },
+  });
+
+let clientQueryClientSingleton: QueryClient | undefined;
+const getQueryClient = () => {
+  if (typeof window === "undefined") {
+    // Server: always make a new query client
+    return createQueryClient();
+  }
+  // Browser: use singleton pattern to keep the same query client
+  clientQueryClientSingleton ??= createQueryClient();
+
+  return clientQueryClientSingleton;
+};
+
+export function ReactQueryProvider({ children }: { children: ReactNode }) {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,13 +1,62 @@
-import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+import { cors } from "hono/cors";
+import { Hono } from "hono";
 
-import { log } from "@markly/utils";
+import { CLIENT_BASE_URL, log } from "@markly/utils";
+import { auth } from "@markly/auth";
 import { env } from "@markly/lib";
 
-const app = new Hono();
+export type AppType = {
+  user: typeof auth.$Infer.Session.user | null;
+  session: typeof auth.$Infer.Session.session | null;
+};
+
+const app = new Hono<{ Variables: AppType }>();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
+});
+
+app.on(["POST", "GET"], "/api/auth/*", (c) => {
+  return auth.handler(c.req.raw);
+});
+
+app.use(
+  "/api/auth/*",
+  cors({
+    origin: CLIENT_BASE_URL,
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["POST", "GET", "OPTIONS"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600,
+    credentials: true,
+  }),
+);
+
+app.use("*", async (c, next) => {
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
+  if (!session) {
+    c.set("user", null);
+    c.set("session", null);
+    return next();
+  }
+
+  c.set("user", session.user);
+  c.set("session", session.session);
+  return next();
+});
+
+app.get("/session", (c) => {
+  const session = c.get("session");
+  const user = c.get("user");
+  if (!user) {
+    return c.body(null, 401);
+  }
+
+  return c.json({
+    session,
+    user,
+  });
 });
 
 const server = serve(
