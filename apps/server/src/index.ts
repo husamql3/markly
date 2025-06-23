c1b673e70fa7bf54ac89b158d55317e49d63d92b
Hüsam <husamahmud@gmail.com>
2025-06-23 16:59:05 +0300

    use t3 env

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,18 +1,35 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 
+import { log } from "@markly/utils";
+import { env } from "@markly/lib";
+
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-serve(
+const server = serve(
   {
     fetch: app.fetch,
-    port: 3000,
+    port: env.SERVER_PORT,
   },
   (info) => {
-    console.log(`Server is running on http://localhost:${info.port}`);
+    log.debug(`http://localhost:${info.port}`);
   },
 );
+
+process.on("SIGINT", () => {
+  server.close();
+  process.exit(0);
+});
+process.on("SIGTERM", () => {
+  server.close((err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    process.exit(0);
+  });
+});
