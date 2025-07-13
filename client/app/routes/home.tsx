e30f79dbafb69f0903f3502a0e12024ee5447212
Hüsam <husamahmud@gmail.com>
2025-07-13 09:53:07 +0300

    add pipeline

--- client/app/routes/home.tsx
+++ client/app/routes/home.tsx
@@ -3,29 +3,29 @@ import { Button } from "@/components/ui/button";
 import { useAuth } from "@/lib/use-auth";
 
 export function meta() {
-	return [
-		{ title: "Markly" },
-		{ name: "description", content: "Welcome to React Router!" },
-	];
+  return [
+    { title: "Markly" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
 }
 
 export default function Home() {
-	const { session, isAuthenticated, isPending, signOut } = useAuth();
+  const { session, isAuthenticated, isPending, signOut } = useAuth();
 
-	if (isPending) {
-		return <div>Loading...</div>;
-	}
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
 
-	return (
-		<div className="h-svh flex justify-center items-center">
-			{isAuthenticated ? (
-				<div>
-					<div>{JSON.stringify(session, null, 2)}</div>
-					<Button onClick={() => signOut()}>Sign Out</Button>
-				</div>
-			) : (
-				<Link to="/login">login</Link>
-			)}
-		</div>
-	);
+  return (
+    <div className="flex h-svh items-center justify-center">
+      {isAuthenticated ? (
+        <div>
+          <div>{JSON.stringify(session, null, 2)}</div>
+          <Button onClick={() => signOut()}>Sign Out</Button>
+        </div>
+      ) : (
+        <Link to="/login">login</Link>
+      )}
+    </div>
+  );
 }
