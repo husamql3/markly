0beb953e4e337411d333107f65503302a0423fdc
Hüsam <husamahmud@gmail.com>
2025-07-13 09:57:47 +0300

    fix build

--- shared/src/types/auth.ts
+++ shared/src/types/auth.ts
@@ -1,4 +1,4 @@
-import type { account, session, user, verification } from "server/dist/db/schema/auth";
+import type { account, session, user, verification } from "../../../server/dist/db/schema/auth";
 
 export type User = typeof user.$inferSelect;
 export type Session = typeof session.$inferSelect;
